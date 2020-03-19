ed0ccb5f04c0146e6477d1fabce97e32d5cbb243
zach-griffin <8115+zach-griffin@users.noreply.github.tamu.edu>
2020-03-19 18:31:27 -0500

    Initial exception handler implementatiom

--- page_table.C
+++ page_table.C
@@ -18,20 +18,22 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool, ContFramePool * _p
   Console::puts("Initialized Paging System\n");
 }
 
+//------------------------------------------------------------------------------------------------
+
 PageTable::PageTable(){
 
-  unsigned long address = 4096 * kernel_mem_pool->get_frames(1); //reserve memory for the page directory
+  unsigned long address = PAGE_SIZE * kernel_mem_pool->get_frames(1); //reserve memory for the page directory
   page_directory = (unsigned long *) address;
 
-  address = 4096 * kernel_mem_pool->get_frames(1);
+  address = PAGE_SIZE * kernel_mem_pool->get_frames(1);
   unsigned long* page_table = (unsigned long*) address;
 
   address = 0;
 
   int i;
-  for(i = 0; i < 1024; i++){
+  for(i = 0; i < ENTRIES_PER_PAGE; i++){
     page_table[i] = address | 3;
-    address = address + 4096;
+    address = address + PAGE_SIZE;
   }
 
   Console::puti(page_table[0]);
@@ -39,20 +41,22 @@ PageTable::PageTable(){
   page_directory[0] = (unsigned long)page_table;
   page_directory[0] = page_directory[0] | 3;
 
-  for(i = 1; i < 1024; i++){
-    page_directory[i] = 0 | 2;
+  for(i = 1; i < ENTRIES_PER_PAGE; i++){
+    page_directory[i] = 2;
   }
-
-  current_page_table = this;
 
   Console::puts("Constructed Page Table object\n");
 }
 
+//------------------------------------------------------------------------------------------------
 
 void PageTable::load(){
    write_cr3((unsigned long)page_directory);
+   current_page_table = this;
    Console::puts("Loaded page table\n");
 }
+
+//------------------------------------------------------------------------------------------------
 
 void PageTable::enable_paging(){
    paging_enabled = 1;
@@ -60,8 +64,43 @@ void PageTable::enable_paging(){
    Console::puts("Enabled paging\n");
 }
 
-void PageTable::handle_fault(REGS * _r){
-  assert(false);
+//------------------------------------------------------------------------------------------------
+
+void PageTable::handle_fault(REGS * _r){ 
+
+
+  Console::puts("\nerr code: ");
+  Console::puti(_r->err_code);
+  Console::puts("\n");
+
+  assert(!(_r->err_code & 1)) //we only want to handle page not found errors
+
+  unsigned long address = read_cr2();
+  unsigned long dir_index = address >> 22; //the page directory index in question
+  unsigned long table_index = (address >> 12) & 0x3FF; //the page table index in question
+
+  Console::puts("\ncr2: ");
+  Console::puti(address);
+  Console::puts("\n");
+
+  //check to see if the page directory is missing an entry
+  if((current_page_table->page_directory[dir_index] & 1) == 0){
+    Console::puts("Adding a page directory entry\n");
+    unsigned long* page_table = (unsigned long*)(PAGE_SIZE * current_page_table->kernel_mem_pool->get_frames(1)); //get a new frame for the page table itself
+    int i;
+    for(int i = 0; i < ENTRIES_PER_PAGE; i++){
+      page_table[i] = 2; //mark each page table entry as not present
+    }
+    current_page_table->page_directory[dir_index] = (unsigned long) page_table; //update the page dirctory
+    current_page_table->page_directory[dir_index] = current_page_table->page_directory[dir_index] | 3; //mark as present
+  }
+
+
+  unsigned long* page_table = (unsigned long*)(current_page_table->page_directory[dir_index] & 0xFFFFF00); //this is the address of the page table pointed to by the given index of the page directory
+  
+  unsigned long new_address = process_mem_pool->get_frames(1) * PAGE_SIZE; //get a new frame for the new page
+
+  page_table[table_index] = new_address | 3; //map the new page to the new frame an set as present
+
   Console::puts("handled page fault\n");
 }
-
